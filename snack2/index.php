15bd95a23a2f7c414e953a52ac72c91c7cc38165
valentina cantarelli <valentinacanterina@gmail.com>
2021-10-19 17:00:53 +0200

    snack 2

--- snack2/index.php
+++ snack2/index.php
@@ -1,8 +1,19 @@
 <?php
-Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) 
-che name sia più lungo di 3 caratteri,che mail contenga un punto e una chiocciola e che age sia un numero.
- Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+// Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) 
+// che name sia più lungo di 3 caratteri,che mail contenga un punto e una chiocciola e che age sia un numero.
+//  Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+
+$name = $_GET["name"];
+$mail = $_GET["mail"];
+$age = $_GET["age"];
+$verifica = false;
 
 
+if(strpos($mail,"@") && strpos($mail, ".") && strlen($name)>3 && is_numeric($age)){
+    $verifica = true;
+    echo "Accesso riuscito";
+}else{
+    echo "Accesso negato";
+}
 
 ?>
